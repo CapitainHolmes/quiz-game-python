839a108d6af41d406e18df3c82616c2904f20269
Tom Holmes <thomasjj.holmes@gmail.com>
2019-04-09 13:31:19 +0000

    first commit

---
+++ quiz.py
@@ -0,0 +1,9 @@
+def show_menu():
+    print("1. Ask Questions")
+    print("2. Add a Question")
+    print("3. Exit Game")
+    
+    option = input("Enter Option: ")
+    return option
+    
+print (show_menu())
